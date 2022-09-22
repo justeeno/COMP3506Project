82170f14f1fa4ab940b8be815e808f3e219f2b62
justeeno <89774668+justeeno@users.noreply.github.com>
2022-09-23 05:30:31 +1000

    asdf

--- Python/Q2/login_system.py
+++ Python/Q2/login_system.py
@@ -94,4 +94,21 @@ class LoginSystem (LoginSystemBase):
             self.login_list[password_correct] = [self.hash_codes(email), self.hash_codes(new_password)]
             return True
 
+# if __name__ == "__main__":
+#     """
+#         !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+#         REMOVE THE MAIN FUNCTION BEFORE SUBMITTING TO THE AUTOGRADER 
+#         !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+#         The following main function is provided for simple debugging only
+#     """
 
+#     login = LoginSystem()
+
+#     assert login.hash_codes("GQHTMP") == login.hash_codes("H2HTN1")
+#     assert len(login) == 101
+#     assert login.check_password("a@b.c", "L6ZS9") == -1
+#     login.add_user("a@b.c", "L6ZS9")
+#     assert login.check_password("a@b.c", "ZZZZZZ") == -2
+#     assert login.check_password("a@b.c", "L6ZS9") == 94
+#     login.remove_user("a@b.c", "L6ZS9")
+#     assert login.check_password("a@b.c", "L6ZS9") == -1
