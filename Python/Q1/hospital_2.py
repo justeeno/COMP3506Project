b2b22a89eb832b5aa9d48db786cafa8d0cb48dc9
justeeno <89774668+justeeno@users.noreply.github.com>
2022-09-23 17:01:44 +1000

    asdf

--- Python/Q1/hospital_2.py
+++ Python/Q1/hospital_2.py
@@ -72,18 +72,18 @@ class Hospital_2 (HospitalBase):
                 new_list = self.append(new_list, og_list[i])
         return new_list
 
-if __name__ == "__main__":
+# if __name__ == "__main__":
 
-    ll = Hospital_2()
-    ll.add_patient(Patient("George", "14:00"))
-    ll.add_patient(Patient("Alex", "13:15"))
-    ll.add_patient(Patient("Max", "11:00"))
-    ll.add_patient(Patient("Justin", "12:00"))
-    ll.add_patient(Patient("Alice", "10:00"))
-    ll.add_patient(Patient("Emily", "10:00"))
-    ll.add_patient(Patient("John", "18:05"))
-    ll.add_patient(Patient("Sid", "08:43"))
+#     ll = Hospital_2()
+#     ll.add_patient(Patient("George", "14:00"))
+#     ll.add_patient(Patient("Alex", "13:15"))
+#     ll.add_patient(Patient("Max", "11:00"))
+#     ll.add_patient(Patient("Justin", "12:00"))
+#     ll.add_patient(Patient("Alice", "10:00"))
+#     ll.add_patient(Patient("Emily", "10:00"))
+#     ll.add_patient(Patient("John", "18:05"))
+#     ll.add_patient(Patient("Sid", "08:43"))
     
-    list_of_patients = [Patient("Sid", "08:43"), Patient("Alice", "10:00"), Patient("Emily", "10:00"), Patient("Max", "11:00"), Patient("Alex", "13:15"), Patient("George", "14:00")]
-    for i, el in enumerate(ll):
-        assert el == list_of_patients[i]
+#     list_of_patients = [Patient("Sid", "08:43"), Patient("Alice", "10:00"), Patient("Emily", "10:00"), Patient("Max", "11:00"), Patient("Alex", "13:15"), Patient("George", "14:00")]
+#     for i, el in enumerate(ll):
+#         assert el == list_of_patients[i]
